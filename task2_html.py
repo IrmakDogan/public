8a763d03271de4b2d8202e5b209056a48719e9a2
irmak-asus <irmakdogan92@gmail.com>
2019-02-19 01:14:50 +0100

    tasks

--- task2_html.py
+++ task2_html.py
@@ -3,7 +3,7 @@ import os.path
 
 
 body = open("task2.html", "w") 
-body.write("<!DOCTYPE html>\n<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n\t<title>In this task, each image is provided with an object bounding box. Please write the expression number that you would be more likely to use to describe this object. Please write -1 for none of them and 0 for either one of them. </title>\n\t<link rel=\"stylesheet\" type=\"text/css\"\n</head>\n<body>\n\n\n<table width=\"85%\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" style=\"margin-top: 30px;\">\n  <tbody><tr> \n    <td> \n\n\n\n<h1 align=\"center\" style=\"font-size: 24pt;\"><b>In this task, each image is provided with an object bounding box. Please write the expression number that you would be more likely to use to describe this object. Please write -1 for none of them and 0 for either one of them.</b></h1><br>\n\n")
+body.write("<!DOCTYPE html>\n<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n\t<title> Task 2 </title>\n\t<link rel=\"stylesheet\" type=\"text/css\"\n</head>\n<body>\n\n\n<table width=\"85%\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\" style=\"margin-top: 30px;\">\n  <tbody><tr> \n    <td> \n\n\n\n<h1 align=\"center\" style=\"font-size: 24pt;\"><b>In this task, each image is provided with an object bounding box. Please write the expression number that you would be more likely to use to describe this object. Please write -1 for none of them and 0 for either one of them.</b></h1><br>\n\n")
 exp = open("./task2/task_expression.txt", "r")
 for i in range(31):
 	body.write("<center><b> <font size=\"65\"> Image "+str(i+1) + ": </b></center>\n")
