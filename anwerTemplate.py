33c9963cd29ee210641b24f378bd85ee302dfdd0
irmak-asus <irmakdogan92@gmail.com>
2019-02-19 15:29:26 +0100

    answers

---
+++ anwerTemplate.py
@@ -0,0 +1,7 @@
+f = open("task1_answer.txt","w")
+for i in range(1,97):
+	f.write("Bounding box number for Image "+str(i)+" : \n")
+	
+f2 = open("task2_answer.txt","w")
+for i in range(1,32):
+	f2.write("Expression number for Image "+str(i)+" : \n")
