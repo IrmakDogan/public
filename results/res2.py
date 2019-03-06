344663365c8aec4d5651fa950124d98a82fe2037
irmak-asus <irmakdogan92@gmail.com>
2019-03-06 23:27:32 +0100

    commits

--- results/res2.py
+++ results/res2.py
@@ -5,7 +5,8 @@ mpl.rcParams['font.size'] = 22
 values = [47.59, 15.18,  25.07,12.16]
 colors = ['lightsalmon', 'lightcyan', 'teal', 'orchid']
 labels = ['Our method ', 'KRREG', 'Both', 'Neither']
-explode = (0.2, 0, 0, 0)
+explode = (0, 0, 0, 0)
 plt.pie(values, colors=colors, labels=labels,  autopct='%1.2f%%', counterclock=False, shadow=True)
-plt.title('The selected referring expression',fontweight='bold',fontsize = 24)
+plt.axis("equal")
+plt.title('The selected referring expression',fontweight='bold',fontsize = 24,y=1.05)
 plt.show()
